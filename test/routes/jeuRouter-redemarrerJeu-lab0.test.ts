df88f1dd50d0715a9656d98150122bb269dd572d
Simon <simon.dion.1@ens.etsmtl.ca>
2025-09-16 16:43:33 -0400

    Étape 3: Écrire des tests pour la fonctionnalité redémarrer

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,30 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import { Joueur } from '../../src/core/joueur';
+import request from "supertest";
+import app from '../../src/app';
+import { jeuRoutes } from '../../src/routes/jeuRouter';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+let joueur1 : Joueur;
+let joueur2 : Joueur;
+
+beforeAll(async () => {
+
+  joueur1 = new Joueur('Simon');
+  joueur2 = new Joueur('Jean');
+});
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it("Scénario principal", async () => {
+    const response = await request(app).get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect((await response).headers['content-type']).toMatch(/json/);
+  });
+
+  it("Aucun joueur restant", async () => {
+    await request(app).get('/api/v1/jeu/redemarrerJeu');
+    const joueurs = JSON.parse(jeuRoutes.controleurJeu.joueurs);
+    expect(joueurs.length).toBe(0);
   });
 });
